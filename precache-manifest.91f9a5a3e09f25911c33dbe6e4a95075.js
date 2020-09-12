7ed8884a3f671a9a3bbab243ef6967fdc8322900
Gautam Arora <gautamarora6248@gmail.com>
2020-09-12 22:23:44 +0530

    Updates

---
+++ precache-manifest.91f9a5a3e09f25911c33dbe6e4a95075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f055d9a4545aa3fecaac74823034559",
+    "url": "/typing-frenzy/index.html"
+  },
+  {
+    "revision": "d3c6acc9bd2861fdc32d",
+    "url": "/typing-frenzy/static/css/main.d81ef22e.chunk.css"
+  },
+  {
+    "revision": "d38f5e3753d348379fde",
+    "url": "/typing-frenzy/static/js/2.aef2b310.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/typing-frenzy/static/js/2.aef2b310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c6acc9bd2861fdc32d",
+    "url": "/typing-frenzy/static/js/main.466c0481.chunk.js"
+  },
+  {
+    "revision": "10dfa5db08026d2767e2",
+    "url": "/typing-frenzy/static/js/runtime-main.8adf8caa.js"
+  }
+]);
